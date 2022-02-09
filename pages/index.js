7ac93d75522500410a251f51e45b1a9047bba7b3
Adela‘s Air <a@bytebase.com>
2022-02-09 17:57:16 +0800

    fix again

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Image from 'next/image'
 const Home = (props) => (
   <div>
     <Head>
-      <title>Adela's Learning Blog</title>
+      <title>Adela&apos;s Learning Blog</title>
     </Head>
     <div className="relative pt-6 lg:pt-8 flex items-center justify-between text-slate-700 font-semibold text-sm leading-6 dark:text-slate-200">
     lalalalal
